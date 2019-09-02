7012f0fdb7cd809323bf04354852f10c3118b884
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-09-02 10:59:55 +0300

    Add http module based on axios

---
+++ src/utils/app/auth.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+class Auth {
+  login(credentials, cb) {
+    axios.post(`${process.env.REACT_APP_SERVER_URL}/auth/login`, credentials)
+      .then((resp) => {
+        const {token} = resp.data.data.user;
+
+        localStorage.setItem('auth', token);
+
+        cb(true, token)
+      })
+      .catch((err) => {
+        cb(false, err);
+      })
+  }
+
+  logout() {
+    localStorage.removeItem('auth');
+  }
+
+  isAuthenticated() {
+    return !!localStorage.getItem('auth');
+  }
+}
+
+export const authHeader = () => {
+  const auth = localStorage.getItem('auth');
+
+  return auth ? {Authorization: `Bearer ${auth}`} : {};
+};
+
+export default new Auth();
---
+++ src/utils/app/http.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import toastr from 'toastr';
+import {authHeader} from "./Auth";
+
+const http = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    ...authHeader(),
+  },
+});
+
+http.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    let err;
+    toastr.remove();
+    if (error.response) {
+      // server error
+      switch (error.response.status) {
+        case 401:
+          // clear user token on local storage
+          localStorage.removeItem('auth');
+          // prevent toaster from displaying if its used by any saga
+          toastr.options.toastClass = 'hide-toast';
+          // redirect to login page
+          window.location.assign('/login');
+          break;
+        case 403:
+          // deal with authorization errors
+          // we don't have to deal with it
+          break;
+        default:
+          break;
+      }
+    } else {
+      // network error
+      // deal with network errors
+      // set a sensible message to be toasted by the consumer
+      err = {
+        response: {
+          status: 'NETWORK_ERR',
+          data: {message: 'Please check your network and try again.'},
+        },
+      };
+    }
+    return Promise.reject(err || error);
+  },
+);
+
+export default http;
