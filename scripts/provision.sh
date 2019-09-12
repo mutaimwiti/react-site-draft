27db34fdd51e847872915bbdfddbd688f489d5de
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-09-12 22:42:16 +0300

    Destroy provisioning script after it has completed its job

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -4,7 +4,8 @@ pm2 stop all
 rm -rf mmdp_website_draft
 source .env
 git clone -b $GIT_BRANCH git@github.com:mutaimwiti/mmdp_website_draft.git
-cp .env mmdp_website_draft/.env
+mv .env mmdp_website_draft/.env
 yarn --cwd mmdp_website_draft
 yarn --cwd mmdp_website_draft build
 pm2 serve --spa mmdp_website_draft/build
+rm $0 # delete this script
