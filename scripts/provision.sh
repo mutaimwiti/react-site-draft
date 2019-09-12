12077e91670588e6d2774fc1d92a3384497a75ad
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-09-12 22:48:59 +0300

    Parse git branch from .env file

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,7 +2,7 @@
 
 pm2 stop all
 rm -rf mmdp_website_draft
-source .env
+GIT_BRANCH=$(grep GIT_BRANCH .env | cut -d '=' -f 2-) # get branch from .env
 git clone -b $GIT_BRANCH git@github.com:mutaimwiti/mmdp_website_draft.git
 mv .env mmdp_website_draft/.env
 yarn --cwd mmdp_website_draft
