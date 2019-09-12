d2d772264d850a05630a69b5724cff121743695b
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-09-12 14:17:52 +0300

    Build and deploy in one job

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 
-if [ "$CIRCLE_BRANCH" == master ]; then
-    # production
-    SERVER_IP=$PROD_IP
+if [ "$CIRCLE_BRANCH" == master ] || [ "$CIRCLE_BRANCH" == develop ]; then
+    if [ "$CIRCLE_BRANCH" == master ]; then
+        # set production env variables and server
+        echo "Deploying to master"
+        SERVER_IP=$PROD_IP
+        echo "REACT_APP_SERVER_URL"=$(echo $PROD_REACT_APP_SERVER_URL) >> .env
+    else
+        # set staging env variables and server
+        echo "Deploying to staging"
+        SERVER_IP=$STAG_IP
+        echo "REACT_APP_SERVER_URL"=$(echo $STAG_REACT_APP_SERVER_URL) >> .env
+    fi
+
+    echo "GIT_BRANCH"=$(echo $CIRCLE_BRANCH) >> .env
+
+    ssh-keyscan $SERVER_IP >> ~/.ssh/known_hosts
+
+    # copy provision.sh to server
+    # provision.sh stops pm2, removes existing work, clones repo, builds app and starts pm2
+    scp scripts/provision.sh $DEPLOY_USER@$SERVER_IP:/home/$DEPLOY_USER/provision.sh
+    # copy .env to server
+    # .env will be used when building the app
+    scp .env $DEPLOY_USER@$SERVER_IP:/home/$DEPLOY_USER/.env
+    # trigger provision.sh
+    ssh $DEPLOY_USER@$SERVER_IP "chmod +x provision.sh && ./provision.sh"
 else
-    # staging
-    SERVER_IP=$STAG_IP
+    echo Skipping deployment of $CIRCLE_BRANCH
 fi
-
-#scp .env $DEPLOY_USER@$SERVER_IP:/home/$DEPLOY_USER/$DEPLOY_FOLDER/.env
-#echo "$DEPLOY_USER$SERVER_IP"
-#ssh -v $DEPLOY_USER@$SERVER_IP "./deploy.sh"
-
-scp .env $DEPLOY_USER@$SERVER_IP:/home/$DEPLOY_USER/$DEPLOY_FOLDER/.env
-ssh $DEPLOY_USER@$SERVER_IP "./deploy.sh"
---
+++ scripts/provision.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+pm2 stop all
+rm -rf mmdp_website_draft
+source .env
+git clone -b $GIT_BRANCH git@github.com:mutaimwiti/mmdp_website_draft.git
+yarn --cwd mmdp_website_draft
+yarn --cwd mmdp_website_draft build
+pm2 serve --spa mmdp_website_draft/build
