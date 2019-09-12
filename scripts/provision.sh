6b9f883c9a910e17fdcdb0f2873f6cd699d9373b
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-09-12 22:57:04 +0300

    Optimize deployment by preparing before moving to new version

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-# stop all pm2 processes
-pm2 stop all
-# remove current repo
-rm -rf mmdp_website_draft
 # get branch name to deploy from .env
 GIT_BRANCH=$(grep GIT_BRANCH .env | cut -d '=' -f 2-)
 # clone branch to deploy from github
-git clone -b $GIT_BRANCH git@github.com:mutaimwiti/mmdp_website_draft.git
+git clone -b $GIT_BRANCH git@github.com:mutaimwiti/mmdp_website_draft.git new_mmdp_website_draft
 # move .env to cloned repo
-mv .env mmdp_website_draft/.env
+mv .env new_mmdp_website_draft/.env
 # install project dependencies
-yarn --cwd mmdp_website_draft
+yarn --cwd new_mmdp_website_draft
 # build a production version
-yarn --cwd mmdp_website_draft build
+yarn --cwd new_mmdp_website_draft build
+# stop all pm2 processes
+pm2 stop all
+# remove old version
+rm -rf mmdp_website_draft
+# make new version current version
+mv new_mmdp_website_draft mmdp_website_draft
 # start app with pm2
 pm2 serve --spa mmdp_website_draft/build
 # delete this script - hopefully it has completed its job :)
