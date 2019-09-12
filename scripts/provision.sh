6d4255d9442d3f35ea873c3b45cdfb54c27312d5
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-09-12 22:37:08 +0300

    Load env variables correctly when provisioning
    - copy .env to project folder before building

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -4,6 +4,7 @@ pm2 stop all
 rm -rf mmdp_website_draft
 source .env
 git clone -b $GIT_BRANCH git@github.com:mutaimwiti/mmdp_website_draft.git
+cp .env mmdp_website_draft/.env
 yarn --cwd mmdp_website_draft
 yarn --cwd mmdp_website_draft build
 pm2 serve --spa mmdp_website_draft/build
